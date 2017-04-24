3902e2535205ca1bc621d2bd5e635edfb03d5068
ehco1996 <zh19960202@gmail.com>
2017-04-24 22:37:48 +0800

    更新reaeme ，指定计划

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-class Student():
-    def __str__(self):
-        return "1"
-    pass
-
-def test(parm='123'):
-    a = dict(parm = Student)
-    print(a[parm]())    
-    return a
-
-test()
+print ('hello the world')
